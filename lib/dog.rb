209a763d411570ca8c9558db960bec68abd22564
Matthew Kloecker <matthew.kloecker@gmail.com>
2017-10-04 20:25:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,17 +11,4 @@ class Dog
     def name=(new_name)
       @name = new_name
     end
-
-    def initialize(breed)
-      @breed = breed
-    end
-
-    def breed
-      @breed
-    end
-
-    def breed=(new_breed)
-      @breed = new_breed
-    end
-
-  end
+end
